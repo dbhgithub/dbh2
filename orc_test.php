a00fa1e4dfc969fe5f8cd3ede8e6345cbcd310d8
dbhgithub <daniel@dbh.com.mx>
2013-11-04 21:10:01 -0600

    Segundo commentario

--- orc_test.php
+++ orc_test.php
@@ -1,5 +1,6 @@
 <?php
-//New comment
+//First comment
+//Second comment
 // Connects to the XE service (i.e. database) on the "localhost" machine
 $conn = oci_connect('dbhsys', 'dbhsys', 'localhost/XE');
 if (!$conn) {
